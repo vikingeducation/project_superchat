0758b10ab4a9074093cca0b2ec422b83bdb6d309
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-05 15:37:04 +0000

    Add notes to README.md

--- README.md
+++ README.md
@@ -2,3 +2,36 @@
 Build a realtime multi-room chat application. Make it super.
 
 Renzo Tomlinson and Will Whitworth
+
+
+
+Notes:
+
+If user has username cookie,
+Send them to the chat page
+If not, then require them to submit a username which sets the cookie
+and redirects them to the chatpage
+
+
+
+They get a default chatroom
+--Get a list of chatrooms that they can click on
+Only one chatroom displayed at a time
+
+
+They can submit posts to the chatroom
+Posts submitted through chatroom are handled with websockets
+New room submissions are handled with websocks
+
+Handle chatroom changes with a post request (partials? Possible to just use websockets)
+
+
+
+
+
+
+
+It should update in realtime on every browser that is connected to the server
+
+
+
--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const path = require("path");
+
+
+//Require routes
 const index = require("./routes/index.js");
+
+
+
+
 
 // Set up handlebars
 const exphbs = require("express-handlebars");
@@ -18,4 +25,29 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", index);
 
-server.listen(3000);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+
+
+
+server.listen(process.env.PATH || 3000);
