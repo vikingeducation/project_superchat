1bb187ff09f0e1878bb16ec2fc9cf7ebd5331e9f
William Whitworth <william10charles@gmail.com>
2017-04-05 10:04:19 -0600

    building html of login

--- app.js
+++ app.js
@@ -3,14 +3,10 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const path = require("path");
-
+const cookieParser = require("cookie-parser");
 
 //Require routes
 const index = require("./routes/index.js");
-
-
-
-
 
 // Set up handlebars
 const exphbs = require("express-handlebars");
@@ -18,6 +14,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Set up body-parser
+app.use(cookieParser());
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -25,12 +22,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", index);
 
-
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -39,15 +33,10 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-
-
-
-
-
-server.listen(process.env.PATH || 3000);
+server.listen(process.env.PORT || 3000);
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   res.render("index");
+
+  // let user = req.cookie.user;
+  // if (user) {
+  //   res.render("chat");
+  // } else {
+  //   res.render("index");
+  // }
 });
 
 module.exports = router;
