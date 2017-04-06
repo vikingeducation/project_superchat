ca706936b6df6c98fc23283aa52f5a169e0fcbe5
nicoasp <nicolas.amaya@gmail.com>
2017-04-06 22:58:11 +0200

    create redis users list

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 let router = express.Router();
+let {createUser} = require('../lib/redis_client');
 
 router.get('/', (req, res) => {
 	res.render("index", {title: 'Superchat'});
@@ -8,8 +9,16 @@ router.get('/', (req, res) => {
 router.post('/login', (req, res) => {
 	let username = req.body.username;
   // create the user in redis
-	res.cookie('username', username);
-	res.redirect('/chatroom');
+  createUser(username)
+  	.then(() => {
+			res.cookie('username', username);
+			res.redirect('/chatroom');
+		})
+  	.catch(() => {
+  		console.log("In route, user not created")
+  		res.redirect('/');
+  	})
+
 })
 
 router.get('/logout', (req, res) => {
