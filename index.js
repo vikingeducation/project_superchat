b695de845682e59176e906cec57bd8dead2dda30
Nicholas Romeo <romeonicholas@gmail.com>
2017-04-06 15:11:41 -0700

    Working with new chat rooms

--- index.js
+++ index.js
@@ -1,99 +1,68 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-const redisClient = require("redis").createClient();
-const expressHandlebars = require("express-handlebars");
-const bodyParser = require("body-parser");
-const ra = require("./lib/ra");
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const redisClient = require('redis').createClient();
+const expressHandlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const ra = require('./lib/ra');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  "/socket.io",
-  express.static(__dirname + "node_modules/socket.io-client/dist/")
+  '/socket.io',
+  express.static(__dirname + 'node_modules/socket.io-client/dist/')
 );
 
 const hbs = expressHandlebars.create({
-  defaultLayout: "main"
+  defaultLayout: 'main'
 });
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-io.on("connection", client => {
-  client.on("createNewRoom", newRoomName => {
+io.on('connection', client => {
+  client.on('createNewRoom', newRoomName => {
     //let user = client.username;
     ra.addRoom(newRoomName);
 
-    io.emit("createdNewRoom", newRoomName);
+    io.emit('createdNewRoom', newRoomName);
   });
 
-  client.on("postMessage", (newMessage, room) => {
+  client.on('postMessage', (newMessage, room) => {
     let user = client.username;
     ra.postMessage(client, newMessage, room);
 
-    io.emit("newMessage", newMessage, user, room);
+    io.emit('newMessage', newMessage, user, room);
   });
 
-  client.on("signUp", newUser => {
+  client.on('signUp', newUser => {
     ra.getUserList(client, newUser);
   });
 
-  client.on("userLoggedOut", currentUser => {
-    currentUser = "user_" + currentUser;
+  client.on('userLoggedOut', currentUser => {
+    currentUser = 'user_' + currentUser;
     ra.destroyUser(currentUser);
   });
 
-  client.on("launchChatRoom", nameOfRoom => {
+  client.on('launchChatRoom', nameOfRoom => {
     var currentRoomMessages = ra.getAllMessages(nameOfRoom);
-
-    // messageID,
-    // 'messageBody',
-    // newMessage,
-    // 'username',
-    // user,
-    // 'roomName',
-    // room
   });
 }); // end of io-on method
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   let roomPromise = ra.getAllRoomNames(); // Promise whose resolve is an array of room names
   let messagePromise = ra.getAllMessages(); // Promise whose resolve is an object filled with message objects
 
   Promise.all([roomPromise, messagePromise])
     .then(values => {
-      console.log(values[1]);
       let roomList = values[0]; // The array of roomnames, the first value in the promise resolve;
-      // let completeMessageList = values[1]; //
-      // var filteredMessageArr = [];
-      // for (var keys in completeMessageList) {
-      //   if (keys.roomName === targetRoomName) {
-      //     console.log(keys);
-      //     filteredMessageArr.push(keys);
-      //   }
-      // }
-
-      //for each message in completeMessageList, look at its room value and add it to an array of messages
-      //for that room
-
-      res.render("index", { roomList });
+      res.render('index', { roomList });
     })
     .catch(reason => {
       console.log(reason);
     });
-
-  // ra.getAllRoomNames().then(roomNames => {
-  //   res.render("index", { roomNames });
-  // });
-  //On landing on / we load all roomnames and messages in panels
-  //in background update
-
-  // ra.getAllMessages().then(messageList => {
-  //   res.render("index", { messageList });
-  // });
 });
 
 server.listen(3000);
