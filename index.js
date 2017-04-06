03e1d67a9a4131e2b9118157830e893358117de2
deven1 <deven@sloan.mit.edu>
2017-04-06 12:40:29 -0400

    added user logout functionality

--- index.js
+++ index.js
@@ -1,86 +1,53 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const redisClient = require('redis').createClient();
-const expressHandlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
-const ra = require('./lib/ra');
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const redisClient = require("redis").createClient();
+const expressHandlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const ra = require("./lib/ra");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  '/socket.io',
-  express.static(__dirname + 'node_modules/socket.io-client/dist/')
+  "/socket.io",
+  express.static(__dirname + "node_modules/socket.io-client/dist/")
 );
 
 const hbs = expressHandlebars.create({
-  defaultLayout: 'main'
+  defaultLayout: "main"
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
-io.on('connection', client => {
-  client.on('postMessage', newMessage => {
+io.on("connection", client => {
+  client.on("postMessage", newMessage => {
     let user = client.username;
     ra.postMessage(client, newMessage);
 
-    io.emit('newMessage', newMessage, user);
+    io.emit("newMessage", newMessage, user);
   });
 
-  client.on('signUp', newUser => {
+  client.on("signUp", newUser => {
     ra.getUserList(client, newUser);
   });
-});
 
-app.get('/', (req, res) => {
-  ra.getAllMessages().then(messageList => {
-    res.render('index', { messageList });
+  client.on("userLoggedOut", currentUser => {
+    currentUser = "user_" + currentUser;
+    ra.destroyUser(currentUser);
   });
-
-  //res.render('index', { messageList });
 });
 
-// function renderHomePage(messageList) {
-//
-// }
+app.get("/", (req, res) => {
+  ra.getAllMessages().then(messageList => {
+    res.render("index", { messageList });
+  });
+});
 
 server.listen(3000);
 
 module.exports = {
   //renderHomePage
 };
-
-//var p = ra.getAllMessages();
-
-// let messagesObj = {};
-//
-// var p = new Promise(function(resolve, reject) {
-//   redisClient.keys('message_*', (err, keys) => {
-//     //console.log(keys);
-//     if (keys.length === 0) {
-//       resolve(messagesObj);
-//     }
-//
-//     keys.forEach(key => {
-//       redisClient.hgetall(key, (err, message) => {
-//         messagesObj[key] = message;
-//         //console.log(Object.keys(messagesObj).length);
-//         //console.log(keys.length);
-//         if (Object.keys(messagesObj).length === keys.length) {
-//           resolve(messagesObj);
-//         }
-//       });
-//     });
-//   });
-//  });
-
-// p.then(messageList => {
-//   res.render('index', { messageList });
-// });
-//
-// function renderHomePage(messageList) {
-//   res.render('index', { messageList });
-// }
