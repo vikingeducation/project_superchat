c0bd9a4fe6b0532477d50d99ed0f1f345ae99fbf
Nicholas Romeo <romeonicholas@gmail.com>
2017-04-06 13:42:01 -0700

    refactor render to use promise.all

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-const redisClient = require("redis").createClient();
-const expressHandlebars = require("express-handlebars");
-const bodyParser = require("body-parser");
-const ra = require("./lib/ra");
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const redisClient = require('redis').createClient();
+const expressHandlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const ra = require('./lib/ra');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  "/socket.io",
-  express.static(__dirname + "node_modules/socket.io-client/dist/")
+  '/socket.io',
+  express.static(__dirname + 'node_modules/socket.io-client/dist/')
 );
 
 const hbs = expressHandlebars.create({
-  defaultLayout: "main"
+  defaultLayout: 'main'
 });
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-io.on("connection", client => {
-  client.on("createNewRoom", newRoomName => {
+io.on('connection', client => {
+  client.on('createNewRoom', newRoomName => {
     //let user = client.username;
     ra.addRoom(newRoomName);
 
-    io.emit("createdNewRoom", newRoomName);
+    io.emit('createdNewRoom', newRoomName);
   });
 
-  client.on("postMessage", (newMessage, room) => {
+  client.on('postMessage', (newMessage, room) => {
     let user = client.username;
     ra.postMessage(client, newMessage, room);
 
-    io.emit("newMessage", newMessage, user, room);
+    io.emit('newMessage', newMessage, user, room);
   });
 
-  client.on("signUp", newUser => {
+  client.on('signUp', newUser => {
     ra.getUserList(client, newUser);
   });
 
-  client.on("userLoggedOut", currentUser => {
-    currentUser = "user_" + currentUser;
+  client.on('userLoggedOut', currentUser => {
+    currentUser = 'user_' + currentUser;
     ra.destroyUser(currentUser);
   });
 
-  client.on("launchChatRoom", nameOfRoom => {
-    currentRoom = nameOfRoom;
-    var allMessages = ra.getAllMessages(nameOfRoom);
+  client.on('launchChatRoom', nameOfRoom => {
+    var currentRoomMessages = ra.getAllMessages(nameOfRoom);
 
     // messageID,
     // 'messageBody',
@@ -60,10 +59,29 @@ io.on("connection", client => {
   });
 }); // end of io-on method
 
-app.get("/", (req, res) => {
-  ra.getAllRoomNames().then(roomNames => {
-    res.render("index", { roomNames });
-  });
+app.get('/', (req, res) => {
+  let roomPromise = ra.getAllRoomNames(); // Promise whose resolve is an array of room names
+  let messagePromise = ra.getAllMessages(); // Promise whose resolve is an object filled with message objects
+
+  Promise.all([roomPromise, messagePromise])
+    .then(values => {
+      console.log(values[1]);
+      let roomList = values[0]; // The array of roomnames, the first value in the promise resolve;
+      let completeMessageList = values[1]; //
+      //for each message in completeMessageList, look at its room value and add it to an array of messages
+      //for that room
+
+      res.render('index', { roomList });
+    })
+    .catch(reason => {
+      console.log(reason);
+    });
+
+  // ra.getAllRoomNames().then(roomNames => {
+  //   res.render("index", { roomNames });
+  // });
+  //On landing on / we load all roomnames and messages in panels
+  //in background update
 
   // ra.getAllMessages().then(messageList => {
   //   res.render("index", { messageList });
