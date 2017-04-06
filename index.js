01e545894908b3bd16ce21b29d88da625c2bace7
Nicholas Romeo <romeonicholas@gmail.com>
2017-04-06 10:12:35 -0700

    Initial work with adding rooms

--- index.js
+++ index.js
@@ -1,49 +1,53 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-const redisClient = require("redis").createClient();
-const expressHandlebars = require("express-handlebars");
-const bodyParser = require("body-parser");
-const ra = require("./lib/ra");
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const redisClient = require('redis').createClient();
+const expressHandlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const ra = require('./lib/ra');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  "/socket.io",
-  express.static(__dirname + "node_modules/socket.io-client/dist/")
+  '/socket.io',
+  express.static(__dirname + 'node_modules/socket.io-client/dist/')
 );
 
 const hbs = expressHandlebars.create({
-  defaultLayout: "main"
+  defaultLayout: 'main'
 });
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-io.on("connection", client => {
-  client.on("postMessage", newMessage => {
+io.on('connection', client => {
+  client.on('postMessage', newMessage => {
     let user = client.username;
     ra.postMessage(client, newMessage);
 
-    io.emit("newMessage", newMessage, user);
+    io.emit('newMessage', newMessage, user);
   });
 
-  client.on("signUp", newUser => {
+  client.on('signUp', newUser => {
     ra.getUserList(client, newUser);
   });
 
-  client.on("userLoggedOut", currentUser => {
-    currentUser = "user_" + currentUser;
+  client.on('userLoggedOut', currentUser => {
+    currentUser = 'user_' + currentUser;
     ra.destroyUser(currentUser);
   });
 });
 
-app.get("/", (req, res) => {
-  ra.getAllMessages().then(messageList => {
-    res.render("index", { messageList });
+app.get('/', (req, res) => {
+  ra.getAllRoomNames().then(roomNames => {
+    res.render('index', { roomNames });
   });
+
+  // ra.getAllMessages().then(messageList => {
+  //   res.render("index", { messageList });
+  // });
 });
 
 server.listen(3000);
