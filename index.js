64e4ebfc637017d8a3aed6998172335944578d31
deven1 <deven@sloan.mit.edu>
2017-04-06 16:09:04 -0400

    created chat room functionality

--- index.js
+++ index.js
@@ -1,55 +1,68 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const redisClient = require('redis').createClient();
-const expressHandlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
-const ra = require('./lib/ra');
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const redisClient = require("redis").createClient();
+const expressHandlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const ra = require("./lib/ra");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  '/socket.io',
-  express.static(__dirname + 'node_modules/socket.io-client/dist/')
+  "/socket.io",
+  express.static(__dirname + "node_modules/socket.io-client/dist/")
 );
 
 const hbs = expressHandlebars.create({
-  defaultLayout: 'main'
+  defaultLayout: "main"
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
-io.on('connection', client => {
-  client.on('createNewRoom', newRoomName => {
+io.on("connection", client => {
+  client.on("createNewRoom", newRoomName => {
     //let user = client.username;
     ra.addRoom(newRoomName);
 
-    io.emit('createdNewRoom', newRoomName);
+    io.emit("createdNewRoom", newRoomName);
   });
 
-  client.on('postMessage', (newMessage, room) => {
+  client.on("postMessage", (newMessage, room) => {
     let user = client.username;
     ra.postMessage(client, newMessage, room);
 
-    io.emit('newMessage', newMessage, user, room);
+    io.emit("newMessage", newMessage, user, room);
   });
 
-  client.on('signUp', newUser => {
+  client.on("signUp", newUser => {
     ra.getUserList(client, newUser);
   });
 
-  client.on('userLoggedOut', currentUser => {
-    currentUser = 'user_' + currentUser;
+  client.on("userLoggedOut", currentUser => {
+    currentUser = "user_" + currentUser;
     ra.destroyUser(currentUser);
   });
-});
 
-app.get('/', (req, res) => {
+  client.on("launchChatRoom", nameOfRoom => {
+    currentRoom = nameOfRoom;
+    var allMessages = ra.getAllMessages(nameOfRoom);
+
+    // messageID,
+    // 'messageBody',
+    // newMessage,
+    // 'username',
+    // user,
+    // 'roomName',
+    // room
+  });
+}); // end of io-on method
+
+app.get("/", (req, res) => {
   ra.getAllRoomNames().then(roomNames => {
-    res.render('index', { roomNames });
+    res.render("index", { roomNames });
   });
 
   // ra.getAllMessages().then(messageList => {
