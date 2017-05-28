470da0c25e0f897dac02b93198cbbde01f408c9f
Egle Libby <eglelibby@gmail.com>
2017-05-28 17:49:55 -0400

    update redis url

--- services/redis/createClient.js
+++ services/redis/createClient.js
@@ -1,4 +1,5 @@
-const REDIS_URL = "//redis://h:pb5c2e26ab92764898753f7d9b135ea957fb475eae0f263579bca67bbd3a49468@ec2-34-206-56-30.compute-1.amazonaws.com:41149"
+const REDIS_URL =
+  "//redis://h:pa7197a8fa17cf6198d9678029a5799998c0ec3723dd70530e027f6f8a1c97188@ec2-34-197-198-120.compute-1.amazonaws.com:34499";
 
 // const redisClient = require("redis").createClient(process.env.REDIS_URL);
 const redisClient = require("redis").createClient();
