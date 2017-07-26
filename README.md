fdf7c8dea7e1d4d7b004e09187a2c9c213f22ab3
Will Timpson <will.timpson@gmail.com>
2017-07-26 10:42:29 -0400

    ALL THE BOILERPLATE

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # project_superchat
 Build a realtime multi-room chat application. Make it super.
+
+Eric and Will
+
+
+
+Rooms:
+    hash: {room-name: number-of-users }
+
+Users:
+    hash: {user-name: room}
+
+Posts:
+    list:
+        room: [author-name, body ,author-name, body, author-name, body,]
---
+++ index.js
@@ -0,0 +1,39 @@
+// Express
+const express = require('express');
+const app = express();
+
+// Socket.io
+const server = require('http').createServer(app);
+const socket = require('./lib/controllers/sockets')(server);
+app.use(
+  '/socket.io',
+  express.static(__dirname + 'node_modules/socket.io-client/dist/')
+);
+
+// Session
+const session = require('express-session');
+app.use(
+  session({
+    secret: 'DzibDjZGbwDzibDjZGbw',
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+// Handlebars
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Static files
+app.use('/public', express.static(`${__dirname}/public`));
+
+// Router
+const router = require('./lib/controllers/routes')(socket);
+app.use('/', router);
+
+// Start server!
+const env = require('./env');
+server.listen(env.port, env.hostname, () => {
+  console.log('Cooking with gas!');
+});
