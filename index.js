f2c9bf4c447b376846581662b7ffdea14cf6f59e
Kara Thrash <dvdonato@gmail.com>
2017-07-26 18:09:08 -0400

    what broke

--- index.js
+++ index.js
@@ -1,63 +1,43 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const hbs = require("express-handlebars");
+const redisClient = require("redis").createClient();
+//redisClient.usernames = redisClient.usernames || [];
 
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-const cookieParser = require("cookie-parser");
-const redisTools = require("./lib/redis_tools");
-const port = 4000;
+const storeUsername = function(username) {
+  return new Promise((resolve, reject) => {
+    redisClient.lpush("usernames", username, (err, reply) => {
+      if (err) reject(err);
+      // User data storage worked correctly
+      // redisClient.end() => do this if not working correctly
 
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.engine("handlebars", hbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-app.use(express.static(`${__dirname}/public`));
-
-let pathname = `${__dirname}/node_modules/socket.io-client/dist/`;
-console.log(pathname);
-app.use("/socket.io", express.static(pathname));
-app.use(bodyParser.urlencoded({ extended: true }));
-
-io.on("connection", client => {
-  client.on("newChatMessage", newMessage => {
-    redisTools.storeMessage(newMessage);
-
-    io.emit("newChatMessageFromServer", newMessage);
+      resolve();
+    });
   });
-});
-
-app.get("/", (req, res) => {
-  if (req.cookies.username) {
-    console.log(`username: ${req.cookies["username"]}`);
-    //go to chat
-    myUserName = req.cookies.username;
-    console.log("Cookie was stored");
-    res.render("chatScreen", { username: myUserName });
-  } else {
-    //cont go to chat //login?
-    //post data//then check the name they enter against names saved
-    res.render("loginScreen");
-  }
-});
-
-app.post("/", (req, res) => {
-  //check username that is entered
-  //TODO: confirm req.body.name as syntax
-  if (!checkUsernameExist(req.body.name)) {
-    res.cookie("username", req.body.name);
-    redisTools.storeUsername(req.body.name);
-    res.redirect("/");
-  } else {
-    console.log("exists");
-  }
-});
-
-var checkUsernameExist = function(name) {
-  return redisTools.getUsernames().includes(name);
 };
 
-server.listen(4000, () => {
-  console.log("Serving!");
-});
+const storeMessage = function(messageObj) {
+  return new Promise((resolve, reject) => {
+    // console.log(messageObj);
+    // console.log(Object.keys(messageObj));
+
+    // redisClient.lpush("messages", {
+    //   username: messageObj.username,
+    //   message: messageObj.message
+    // });
+    redisClient.lpush("messages", messageObj.message);
+    if (err) reject(err);
+    console.log(redisClient.messages);
+    resolve();
+  });
+};
+
+const getMessages = function() {};
+
+var getUsernames = () => {
+  return redisClient.usernames;
+};
+storeUsername.usernames = redisClient.usernames;
+
+module.exports = {
+  storeUsername,
+  getUsernames,
+  storeMessage
+};
