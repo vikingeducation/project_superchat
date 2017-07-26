aace93f54a6d11107793786ede56e7cf9594c45c
Greg Filipczak <gregfilipczak@gmail.com>
2017-07-26 20:29:49 +0000

    finished log out

--- index.js
+++ index.js
@@ -4,31 +4,44 @@ const app = express();
 const hbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const redis = require("./lib/redisWrapper.js");
-
+const cookieParser = require("cookie-parser");
 const server = http.createServer(app);
 const io = require("socket.io")(server);
 app.engine("handlebars", hbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   "/socket.io",
   express.static(__dirname + "/node_modules/socket.io-client/dist/")
 );
 
-// server.listen(3000)
-
-// app.use(
-//   "/socket-",
-//   express.static(__dirname + "node_modules/socket.io-client/dist/")
-// );
-
 app.get("/", (req, res) => {
-  redis.loadMessages(messages => {
-    res.render("index", { messages: messages });
-  });
+  if (!req.cookies.user) {
+    res.render("login");
+  } else {
+    redis.loadMessages(messages => {
+      res.render("index", {
+        messages: messages,
+        user: req.cookies.user
+      });
+    });
+  }
 });
 
-// let messageIDs = [1, 2];
+app.post("/", (req, res) => {
+  let user = req.body.user;
+  redis.saveUser(user);
+  res.cookie("user", user);
+
+  res.redirect("/");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("user");
+  res.redirect("/");
+});
 
 io.on("connection", client => {
   console.log("New connection");
@@ -37,22 +50,10 @@ io.on("connection", client => {
     var p = redis.saveMessage(data.body, data.author, data.room);
 
     p.then(() => {
-    	console.log("Promise found!");
-    	io.emit("updateMessages", data);
+      console.log("Promise found!");
+      io.emit("updateMessages", data);
     });
   });
 });
 
-
-// redis.saveMessage("Hi there", "me", "main-room");
-
-// function parseMessages = (messageArray){
-//   messageArray.forEach((message)=> {
-//
-//   }
-// }
-
-// app.listen(3000, (req, res) => {
-//   console.log("listening on port 3000");
-// });
 server.listen(3000);
