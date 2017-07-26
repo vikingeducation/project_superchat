055dfa349a32afa066a7b04efd7e32ca75fd89b3
Benjamin Soung <soungbenjamin@gmail.com>
2017-07-26 17:23:56 -0400

    showing message on view via sockets

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,15 +2,17 @@ $(_pageInit);
 
 function _pageInit() {
 	// Connect to our backend.
-	const socket = io.connect('http://localhost:3000');
+	const socket = io.connect("http://localhost:3000");
 
 	// Assign event handlers.
-	socket.on('message_sent', data => {
-		document.write(data.body);
+	socket.on("message_sent", data => {
+		let $el = $("<h6>", { text: `${data.body}` });
+
+		$("#message-container").append($el);
 	});
 
-	socket.emit('send_message', {
-		body: 'A seriouisly cool message',
+	socket.emit("send_message", {
+		body: "A seriouisly cool message",
 		user_id: 0,
 		room_id: 0
 	});
