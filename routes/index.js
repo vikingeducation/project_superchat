2b2f578f007948cc7b47332e0901aa68eb5c4f1e
Andrew Senner <me@andrewsenner.com>
2017-07-26 11:06:17 -0400

    users module progress

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const { loadModule, saveModule } = require('../lib/redis_wrapper');
+const { getUsers } = loadModule;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	getUsers(0).then(console.log);
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
