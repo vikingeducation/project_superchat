c74d1329a006a9296e71ee92f0e841ca12b2f5a0
Eric Glover <etgt28@mail.missouri.edu>
2017-07-26 15:39:39 -0500

    events firing, post loading, crazy biz

--- public/scripts.js
+++ public/scripts.js
@@ -1,22 +1,23 @@
 $(chat);
 // global socket for testing
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect("http://localhost:3000");
 
 function chat() {
   //let socket = io.connect("http:localhost/3000");
 
   // Add a new room to the page
-  socket.on('addRoom', roomName => {
+  socket.on("addRoom", roomName => {
     actions.buildRoom(roomName);
   });
 
   // Add a new post to a room
-  socket.on('addPost', messageObj => {
+  socket.on("addPost", messageObj => {
+    //console.log(messageObj);
     actions.buildPost(messageObj);
   });
 
   // User submits a new post, build it!
-  $('#rooms').on('click', 'button', event => {
+  $("#rooms").on("click", "button", event => {
     event.preventDefault();
     actions.newPost(event);
   });
@@ -25,42 +26,43 @@ function chat() {
 let actions = {
   buildRoom: function(roomName) {
     // Create a room with some jQuery magics
-    let article = $('<article>')
-      .addClass('col-md-4 col-lg-3')
-      .attr('data-id', roomName);
-    let h = $('<h2>').text(roomName);
-    let ul = $('<ul>');
-    let form = $('<form>');
-    let section = $('<section>').addClass('form-group');
-    let label = $('<label>').attr('for', 'post');
-    let text = $('<textarea>').attr('name', 'post');
-    let button = $('<button>')
-      .addClass('btn btn-submit post-button')
-      .text('Post something plz')
-      .attr('type', 'button');
+    let article = $("<article>")
+      .addClass("col-md-4 col-lg-3")
+      .attr("data-id", roomName);
+    let h = $("<h2>").text(roomName);
+    let ul = $("<ul>");
+    let form = $("<form>");
+    let section = $("<section>").addClass("form-group");
+    let label = $("<label>").attr("for", "post");
+    let text = $("<textarea>").attr("name", "post");
+    let button = $("<button>")
+      .addClass("btn btn-submit post-button")
+      .text("Post something plz")
+      .attr("type", "button");
     section.append(label).append(text);
     form.append(section).append(button);
     article.append(h).append(ul).append(form);
-    $('#rooms').append(article);
+    $("#rooms").append(article);
   },
   buildPost: function(messageObj) {
     // Create a post with some jQuery magics
-    let li = $('<li>');
-    let title = $('<h3>').text(messageObj.author);
-    let body = $('<p>').text(messageObj.message);
+    let li = $("<li>");
+    let title = $("<h3>").text(messageObj.author);
+    let body = $("<p>").text(messageObj.message);
     li.append(title).append(body);
     let targetText = '[data-id="' + messageObj.roomName + '"]';
-    $(targetText).children('ul').append(li);
+    console.log(targetText);
+    $(targetText).children("ul").append(li);
   },
   newPost: function(event) {
     // Get post details, trigger server event
     let $target = $(event.target);
-    let roomName = $target.closest('article').attr('data-id');
-    let author = 'anon'; //change later
-    let $textBox = $target.siblings('section').children('textarea');
+    let roomName = $target.closest("article").attr("data-id");
+    let author = "anon"; //change later
+    let $textBox = $target.siblings("section").children("textarea");
     let message = $textBox.val();
-    $textBox.val('');
-    socket.emit('addPost', roomName, author, message);
+    $textBox.val("");
+    socket.emit("addPost", roomName, author, message);
   }
 };
 
