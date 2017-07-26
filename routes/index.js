a65519768b5abab8d67cb3561af6ad4b346fda24
Benjamin Soung <soungbenjamin@gmail.com>
2017-07-26 12:34:35 -0400

    working on helper functions for redis wrapper

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,29 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const { loadModule, saveModule } = require('../lib/redis_wrapper');
-const { getModelData, getMessagesByRoomId } = loadModule;
+const { loadModule, saveModule } = require("../lib/redis_wrapper");
+const {
+	getUsers,
+	getMessages,
+	getRooms,
+	getMessagesByRoomId,
+	getUsersByRoomId
+} = loadModule;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	//getModelData('users').then(console.log);
+router.get("/", function(req, res, next) {
+	// getUsers().then(console.log);
+
+	// getMessages().then(console.log);
+	// getRooms().then(console.log);
+
 	// getMessagesByRoomId(0).then(console.log, err => {
 	// 	console.log(err.stack);
 	// });
-	res.render('index', { title: 'Express' });
+
+	getUsersByRoomId(0);
+
+	res.render("index", { title: "Express" });
 });
 
 module.exports = router;
