164ba05784fb6fbef4c8308814607cec45ee59df
Alex-Willenbrink <awillenbrink@gmail.com>
2017-07-26 12:07:43 -0500

    trying username stuff

--- index.js
+++ index.js
@@ -6,32 +6,54 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const cookieParser = require("cookie-parser");
 const port = 4000;
+const redisTools = require("./lib/redis_tools");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.engine("handlebars", hbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-let cookieArray = [];
 var myUserName = "Word Eater was Here";
+
 app.get("/", (req, res) => {
-  console.log(req.cookie);
-  if (req.cookie !== undefined) {
+  if (req.cookies.username) {
+    console.log(`username: ${req.cookies["username"]}`);
     //go to chat
-    myUserName = req.cookie.username;
+    myUserName = req.cookies.username;
+    console.log("Cookie was stored");
     res.render("chatScreen");
   } else {
+
     //cont go to chat //login?
     //post data//then check the name they enter against names saved
     res.render("loginScreen");
   }
 });
+
+
 app.post("/", (req, res) => {
   //check username that is entered
   //TODO: confirm req.body.name as syntax
-  console.log(req.body.name);
-  res.cookie["username"] = req.body.name;
+  if(!checkUserNameExist(req.body.name)) {
+    res.cookie("username", req.body.name);
+
+    res.redirect('/');
+  } else {
+    window.alert("Your username exists");
+    res.end();
+  }
+
+
   console.log(res.cookie["username"]);
+
 });
+
+const checkUsernameExist = ( name ) => {
+  return redisTools.getUsernames().includes(name);
+}
+
+
+
+
 app.listen(port, () => {
   console.log("Serving!");
 });
