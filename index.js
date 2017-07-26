9006d4422967f1409133d0f33b442bbf9e55b781
Malyar Baron <malbaron0@gmail.com>
2017-07-26 17:47:23 -0400

    In progress

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const data = require('./services/chatData.js');
 const express = require('express');
 const app = express();
 
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+let serverSocket;
 
 //set up handlebars
 const exphbs = require('express-handlebars');
@@ -18,17 +21,28 @@ app.get('/', (req, res) => {
     let messagesArr = [];
 
     data.getMessages('test')
+
     .then (messages => {
         messages.map(element => {
             messagesArr.push(JSON.parse(element));
         });
 
-         res.render('index', {"messagesArr": messagesArr});
+        res.render('index', {"messagesArr": messagesArr});
 
     })
+
     .catch((reject) => {
         console.log(reject);
     });
+});
+
+
+io.on('connection', client => {
+
+    client.on('addMsg', (msgProfile) => {
+        data.storeMessages(msgProfile.msg, msgProfile.author);
+    });
+
 });
 
 app.listen(3000);
