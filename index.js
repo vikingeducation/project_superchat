467126cef15e31933550141367102c089302bcd1
Greg Filipczak <gregfilipczak@gmail.com>
2017-07-26 15:06:04 +0000

    working on redis wrapper

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 const hbs = require("express-handlebars");
 const bodyParser = require("body-parser");
-const redisClient = require("redis");
+const redis = require("./lib/redisWrapper.js");
 
 const server = http.createServer(app);
 const io = require("socket.io")(server);
+
+let messageIDs = [1, 2];
+
+redis.loadMessages(messageIDs);
