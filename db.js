f2eab3b079780b382d364888979c3de1de2924a8
Andrew Senner <me@andrewsenner.com>
2017-07-27 19:36:42 -0400

    finished

--- db.js
+++ db.js
@@ -3,12 +3,6 @@ const { saveUser, saveMessage, saveRoom } = saveModule;
 
 const { hashSync: encodePassword } = require('bcryptjs');
 
-saveUser({
-	username: 'Andrew',
-	password: '12345',
-	room_id: -1
-});
-
 for (let r = 0; r < 5; r++) {
 	saveRoom({
 		roomname: `An Awesome Room ${r + 1}`
