2ced3dec7fdaf619dcb94e36222d86924c663d93
Tyler Ketron <tketron@gmail.com>
2017-07-31 21:37:22 -0700

    added login info and storing username as a cookie

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const io = require('socket.io')(server);
 const expressHandlebars = require('express-handlebars');
 const hbsHelpers = require('./helpers/handlebars-helpers');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const { storeMessage, getMessages } = require('./message-store');
 
 app.engine(
@@ -20,29 +21,52 @@ app.use(
 );
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const roomName = 'BASIC';
-const userName = 'Anonymous';
 
 app.get('/', (req, res) => {
-  getMessages(roomName).then(values => {
-    Promise.all(values)
-      .then(messages => {
-        console.log(messages);
-        res.render('index', { messages: messages });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
+  if (req.cookies.username) {
+    getMessages(roomName).then(values => {
+      Promise.all(values)
+        .then(messages => {
+          console.log(messages);
+          res.render('index', {
+            messages: messages,
+            username: req.cookies.username
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  let username = req.body.username;
+  res.cookie('username', username);
+  res.redirect('/');
+});
+
+app.get('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/');
 });
 
 app.post('/', (req, res) => {
   let messageBody = req.body.message;
-  storeMessage(messageBody, userName, roomName);
+  let username = req.cookies.username;
+  storeMessage(messageBody, username, roomName);
   io.sockets.emit('new message', {
     body: messageBody,
-    author: userName,
+    author: username,
     room: roomName
   });
   res.redirect('back');
