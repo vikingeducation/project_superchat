dd28a4ec53858780289fee955e85d5e0767e11d2
Chuck Michael <chuckqs@yahoo.com>
2017-08-30 21:54:59 -0400

    added socketio features

--- index.js
+++ index.js
@@ -9,11 +9,18 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+const chatRooms = require("./modules/chatRooms");
+
 app.use(
   "/socket.io",
   express.static(__dirname + "node_modules/socket.io-client/dist/")
 );
+//Set Directory for public folder
 app.use(express.static(__dirname + "/public"));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Set up handlebars
 const exphbs = require("express-handlebars");
@@ -24,7 +31,33 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  Promise.all([
+    chatRooms.getRoomMessages("Cats"),
+    chatRooms.getRoomAuthors("Cats")
+  ]).then ( (values)=> {
+    let roomMessages = values[0]
+    let roomAuthors = values[1]
+    res.render("index", {roomMessages, roomAuthors});
+  })
+
+});
+
+app.post("/newMessage", (req,res) => {
+  // chatRooms.setRoomMessage(req.body.textNewMessage, "Cats");
+  // chatRooms.setRoomAuthor("Me", "Cats");
+  res.redirect("back");
 })
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+    socket.on('newMessageSubmit', function(data) {
+      chatRooms.setRoomMessage(data.newEnteredMessage, "Cats");
+      chatRooms.setRoomAuthor(data.newEnteredUser, "Cats");
+      io.emit('newMessage', {data})
+    })
+  })
 
 server.listen(3000);
---
+++ modules/chatRooms.js
@@ -0,0 +1,76 @@
+const redisClient = require("redis").createClient();
+
+var redisClientObject = {
+  setUser: function(name) {
+    redisClient.LPUSH("users", name);
+  },
+
+  getUsers: function() {
+    return new Promise ((resolve, reject) => {
+      redisClient.LRANGE("users", 0, -1, function(err,res) {
+        if (err) {
+          reject(err);
+        }
+        else {
+          resolve(res);
+        }
+      });
+    })
+  },
+
+  setRoom: function(name) {
+    redisClient.LPUSH("rooms", name);
+  },
+
+  getRooms: function() {
+    return new Promise ((resolve, reject) => {
+      redisClient.LRANGE("rooms", 0, -1, function(err,res) {
+        if (err) {
+          reject(err);
+        }
+        else {
+          resolve(res);
+        }
+      });
+    })
+  },
+
+  setRoomMessage: function(msg, roomName) {
+    redisClient.LPUSH( ("roomMessages" + roomName) , msg);
+  },
+
+  getRoomMessages: function(roomName) {
+    return new Promise ((resolve, reject) => {
+      redisClient.LRANGE( ("roomMessages" + roomName), 0, -1, function(err,res) {
+        if (err) {
+          reject(err);
+        }
+        else {
+          resolve(res);
+        }
+      });
+    })
+  },
+
+  setRoomAuthor: function(name, roomName) {
+    redisClient.LPUSH( ("roomAuthors" + roomName) , name);
+  },
+
+  getRoomAuthors: function(roomName) {
+    return new Promise ((resolve, reject) => {
+      redisClient.LRANGE( ("roomAuthors" + roomName), 0, -1, function(err,res) {
+        if (err) {
+          reject(err);
+        }
+        else {
+          resolve(res);
+        }
+      });
+    })
+  }
+
+
+
+}
+
+module.exports = redisClientObject;
