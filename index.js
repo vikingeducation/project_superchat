2e1ed47f58e905c6ece37b8bdb798bf247b30d7e
maddiereddy <maddiereddy@gmail.com>
2017-10-17 23:06:20 -0700

    completed warmup

--- index.js
+++ index.js
@@ -4,23 +4,44 @@ const bodyParser = require('body-parser');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const { newPost, getAllPosts } = require('./redisDataStore');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const hbs = expressHandlebars.create({defaultLayout: 'main'});
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/socket.io", express.static(__dirname + "node_modules/socket.io-client/dist/"));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-  res.send("Let's Chat!");
+app.get('/', function(req, res) {
+  res.render('index');
 });
 
-app.get('/chat', function(req, res) {
-  res.render('index', {title: "Let's Begin!"});
+app.post('/update', (req, res) =>{
+  let message = req.body.newPost;
+  let p1 = newPost(message);
+  let p2 = getAllPosts('Main');
+
+  Promise.all([p1, p2]).then(values => {
+    let messages = values[1];
+    io.sockets.emit('new message', messages);
+    res.redirect('back');
+  });
+});
+
+io.on('connection', client => {
+  let p1 = getAllPosts('Main');
+
+  p1.then(messages => {
+    client.emit('new message', messages);
+  });
+
+  client.on('send message', () =>{});
 });
 
 server.listen(4700, () => {
-  console.log("Check out localhost:4700 for my awesome Super Chat app!");
+  console.log("Check out my awesome Super Chat app  at localhost:4700 !!");
 });
---
+++ redisDataStore.js
@@ -0,0 +1,39 @@
+const redis = require('redis');
+const redisClient = redis.createClient();
+
+redisClient.del('main');
+
+const newPost = (message) => {
+  let post = {
+    user: 'Maddie',
+    text: message,
+    date: Date.now(),
+    chatroom: 'Main'
+  };
+  return new Promise((resolve, reject) => {
+    redisClient.lpush(post.chatroom, JSON.stringify(post), (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+const getAllPosts = (chatroom) => {
+  return new Promise((resolve, reject) => {
+    redisClient.lrange(chatroom, 0, -1, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+module.exports = {
+  newPost,
+  getAllPosts
+};
