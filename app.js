9e914262dbd26521ff68bf0706313f4e3c97aee6
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2017-10-31 10:05:01 +0100

    user signin/signout

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const expressHandlebars = require('express-handlebars');
-const { getMessage, addMessage, getAllMessages } = require('./models/message');
+const { addMessage, getAllMessages } = require('./models/message');
+const config = require('./config');
 
 const app = express();
 const server = require('http').createServer(app);
@@ -10,9 +13,14 @@ const io = require('socket.io')(server);
 const host = 'localhost';
 const port = 3000;
 
+app.use(express.static(`${__dirname}/public`));
 app.use('/socket.io', express.static(`${__dirname}node_modules/socket.io-client/dist/`));
-
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: config.keys,
+}));
 
 const hbs = expressHandlebars.create({
   defaultLayout: 'main',
@@ -22,15 +30,26 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
+  const { name } = req.cookies;
   getAllMessages((messages) => {
-    res.render('index', { messages });
+    res.render('index', { name, messages });
   });
 });
 
-app.post('/', (req, res) => {
-  const { messageBody } = req.body;
+app.get('/signout', (req, res) => {
+  res.clearCookie('name');
+  res.redirect('/');
+});
+
+app.post('/user', (req, res) => {
+  const { username } = req.body;
+  res.cookie('name', username);
+  res.redirect('back');
+});
+
+app.post('/message', (req, res) => {
+  const { author, messageBody } = req.body;
   const room = 'cats';
-  const author = 'Anon';
   addMessage(room, author, messageBody);
   const message = {
     author,
---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+  keys: ['asdf1234567890qwer'],
+};
+
+module.exports = config;
