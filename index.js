f95261a0a1444773fb669918f950bc4c0fe3e419
DC <heyyodc-git@yahoo.com>
2017-11-09 21:55:58 -0800

    using handlebars to loop. is working well

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const io = require("socket.io")(server);
 
 const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser");
-// const redis = require("./lib/redis-lib");
+const redis = require("./lib/redis-lib");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set("views", __dirname + "/views");
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
@@ -21,13 +21,33 @@ app.use(express.static(__dirname + "/public"));
 
 //register routes
 app.get("/", (req, res) => {
-	res.render("results", { test: "test content" });
+	//get allChatroomNames into array
+	let chatRooms,
+		memberCounts = [];
+	redis
+		.getSortedItems("zchatrooms")
+		.then(data => {
+			chatRooms = data;
+			console.log(chatRooms);
+			return redis.getMessage("chatroomMemberCounter");
+		})
+		.then(data => {
+			memberCounts = data;
+			console.log(memberCounts);
+			res.render("results", {
+				chatRooms: chatRooms,
+				memberCounts: memberCounts
+			});
+		})
+		.catch(err => {
+			console.log(err);
+		});
 });
 
 io.on("connection", socket => {
 	console.log("a user connected");
 	socket.on("disconnect", () => {
-		console.log("user disconnected");
+		console.log("--> user disconnected");
 	});
 });
 
