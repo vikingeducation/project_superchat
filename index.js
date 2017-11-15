459ff5903f72be90b92d7d9fb9be8985552e33ac
DC <heyyodc-git@yahoo.com>
2017-11-14 22:11:21 -0800

    user functionality and count working with websockets

--- index.js
+++ index.js
@@ -12,15 +12,13 @@ const expressSession = require("express-session");
 
 const PORT = process.env.PORT || 3000;
 
+//setup handlebars
 app.set("views", __dirname + "/views");
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use(bodyParser.urlencoded({ extended: false }));
 //setup middleware
-//attach to req.session
-//to destroy session: req.session = null
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 // this code may not be useful
 // see here for app.use() usage: https://expressjs.com/en/api.html#path-examples
@@ -29,6 +27,8 @@ app.use(
 	express.static(__dirname + "node_modules/socket.io-client/dist/")
 );
 //nice to have / do later: store session in redis, not memory
+//attach to req.session
+//to destroy session: req.session = null
 app.use(
 	expressSession({
 		name: "dcsession",
@@ -41,8 +41,7 @@ app.use(
 
 //register routes
 app.get("/", (req, res) => {
-	console.log("$$$$$ IN route!!");
-	//get allChatroomNames into array
+	console.log("In route........................");
 	// if (req.session) {
 	// 	console.log("SESSION");
 	// 	console.log(req.session);
@@ -53,41 +52,38 @@ app.get("/", (req, res) => {
 	// } else {
 	// 	console.log("NO SESSION");
 	// }
-
-	// let whoseOnline = [];
-	// let countOnline = 0;
-	// redis
-	// 	.getSortedItems("whoseOnline")
-	// 	.then(data => {
-	// 		whoseOnline = data;
-	// 		console.log(whoseOnline);
-	// 		return redis.getCount("whoseOnline");
-	// 	})
-	// 	.then(data => {
-	// 		countOnline = data;
-	// 		res.render("results", {
-	// 			whoseOnline: whoseOnline,
-	// 			countOnline: countOnline
-	// 		});
-	// 	})
-	// 	.catch(err => {
-	// 		console.log(err);
-	// 	});
 	res.render("results", {});
 });
 
 io.on("connection", socket => {
+	console.log("In connection ........................");
+	// if (req.session) {
+	console.log("CONNECTION: socket.username = " + socket.username);
 	let whoseOnline = [];
 	let onlineCount = 0;
 	console.log("a user connected");
 	socket.on("disconnect", () => {
+		console.log("DISCONNECTION: socket.username = " + socket.username);
 		// if (!socket.username) {
 		// return;
 		// }
 		redis
 			.removeSortedItem("whoseOnline", socket.username)
 			.then(data => {
-				console.log(data);
+				console.log("in promise removeSortedItem, num removed = " + data);
+				console.log(
+					"in promise removeSortedItem, socket.username = " + socket.username
+				);
+				console.log("in promise removeSortedItem, whoseOnline= " + whoseOnline);
+				return redis.getCount("whoseOnline");
+			})
+			.then(data => {
+				let count = data;
+				io.emit("get count", count);
+				return redis.getSortedItems("whoseOnline");
+			})
+			.then(data => {
+				let whoseOnline = data;
 				io.emit("get logins", whoseOnline);
 			})
 			.catch(err => {
@@ -102,15 +98,12 @@ io.on("connection", socket => {
 
 	socket.on("new login", (data, callback) => {
 		callback(true);
-		//create session here??
 		socket.username = data;
 
-		console.log("socket.username = " + socket.username);
-		console.log("$$$$$ IN SOCKETS!!");
+		console.log("NEW LOGIN: socket.username = " + socket.username);
 		redis
 			.addSortedItem("whoseOnline", socket.username)
 			.then(data => {
-				console.log("after addSortedItem socket.username: " + data);
 				return redis.getCount("whoseOnline");
 			})
 			.then(data => {
@@ -119,7 +112,7 @@ io.on("connection", socket => {
 			})
 			.then(data => {
 				whoseOnline = data;
-				console.log("!!!@@@@@@@ whoseOnline[] = ", whoseOnline);
+				console.log("whoseOnline[] = ", whoseOnline);
 				io.emit("new login", socket.username);
 				io.emit("get logins", whoseOnline);
 				io.emit("get count", onlineCount);
@@ -127,13 +120,12 @@ io.on("connection", socket => {
 			.catch(err => {
 				console.log(err);
 			});
-
-		//ie, io.sockets.emit('get users', usernames);
 	});
 
 	socket.on("new message", data => {
 		let msg = data;
 		let username = socket.username;
+		console.log("NEW MESSAGE: socket.username = " + socket.username);
 		// let username = "anon";
 		io.emit("new message", msg, username);
 	});
