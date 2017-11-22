193fe885573b535aea9947386c6ad65a1405a5c9
GeneTinderholm <tinderholmgene@gmail.com>
2017-11-22 11:01:42 -0600

    first switch express install

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const redisClient = require('redis').createClient();
+app.engine
 
 app.get("/", (req, res) => {
   res.render()
 })
 
-
-
-
+server.listen(3000);
