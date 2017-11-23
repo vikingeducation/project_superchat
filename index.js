2222941eb0370f2a3a6211b35f1d596a2bb7c6a5
Austin1780 <austinwilliamsmith@gmail.com>
2017-11-22 17:04:24 -0700

    completed promises

--- index.js
+++ index.js
@@ -5,27 +5,30 @@ const bodyParser = require("body-parser");
 const redisClient = require("redis").createClient();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+const cookieParser = require("cookie-parser");
 
-const hbs = expressHandlebars.create({
-  partialsDir: "views/",
-  defaultLayout: "main"
-});
+const hbs = expressHandlebars.create({ defaultLayout: "main" });
+let chatRooms = ["Cats", "Dogs", "Programmers"];
+let newUsername = "";
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(`${__dirname}/public`));
+app.use(cookieParser());
 app.use(
   "/socket.io",
   express.static(__dirname + "node_modules/socket.io-client/dist/")
 );
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(`${__dirname}/public`));
-
-let chatRooms = ["Cats", "Dogs", "Programmers"];
-
 app.get("/", (req, res) => {
-  res.render("index", { chatRooms: chatRooms });
+  if (req.cookies.nameInput) {
+    res.render("index", { chatRooms: chatRooms });
+  } else {
+    newUsername = req.cookies.nameInput;
+    console.log(req.cookies.nameInput);
+    res.render("newUser");
+  }
 });
 
 app.get("/:room", (req, res) => {
@@ -35,21 +38,40 @@ app.get("/:room", (req, res) => {
   });
 });
 
+app.post("/", (req, res) => {
+  res.cookie("nameInput", req.params.nameInput);
+  console.log(req.cookies.nameInput);
+  res.redirect("/");
+});
+
 io.on("connection", client => {
   client.on("submit", data => {
-    redisClient.hmset(
-      "message",
-      "user",
-      "Anon",
-      "userMessage",
-      data,
-      (error, result) => {
-        if (error) res.send("Error: " + error);
-        redisClient.hgetall("message", (err, object) => {
-          io.emit("messages", object);
+    let setCommentPromise = data => {
+      return new Promise((resolve, reject) => {
+        redisClient.hmset(
+          "message",
+          "user",
+          newUsername,
+          "userMessage",
+          data,
+          (err, result) => (err ? reject(err) : resolve(result))
+        );
+      });
+    };
+
+    setCommentPromise(data)
+      .then(result => {
+        return new Promise((resolve, reject) => {
+          redisClient.hgetall(
+            "message",
+            (err, object) => (err ? reject(err) : resolve(object))
+          );
         });
-      }
-    );
+      })
+      .then(result => {
+        io.emit("messages", result);
+      })
+      .catch(err => io.emit("error", err));
   });
 });
 
