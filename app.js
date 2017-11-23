9f21eee86726b9ce049c8f7ea5596f7fe013a3ff
Steven Li <stevenli@Stevens-MacBook-Pro.local>
2017-11-22 16:00:34 -0800

    new push

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ var io = require("socket.io")(server);
 const redisClient = require("redis").createClient();
 const expressHandlebars = require("express-handlebars");
 const cookieParser = require("cookie-parser");
+
 //morgan toolkit
+// const morgan = require("morgan");
+// const morganToolkit = require("morgan-toolkit")(morgan);
+
 app.use(cookieParser());
 
+// app.use(morganToolkit());
+
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
 redisClient.on("error", function(err) {
   console.log("Error " + err);
@@ -133,46 +139,54 @@ app.post("/chatroom/newpost", (req, res) => {
 app.get("/chatroom/:chatroomName", (req, res) => {
   let firstmessageArrPromise = () => {
     return new Promise((resolve, reject) => {
-      if (firstmessageArr.length === 0) {
-        redisClient.KEYS("*", (err, keys) => {
-          keys.forEach(key => {
-            redisClient.hgetall(key, (err, obj) => {
-              firstmessageArr.push(obj);
-            });
-          });
-        });
-      }
-      return resolve(firstmessageArr);
+      resolve();
     });
   };
 
   let chatroomMessagesArr;
+  let allMessageChatrooms;
+  let uniqueChatroomsArr;
 
   firstmessageArrPromise()
-    .then(firstmessageArr => {
+    .then(data => {
+      if (firstmessageArr.length === 0) {
+        console.log("firstmessageArr.length === 0 ran");
+        redisClient.KEYS("*", (err, keys) => {
+          keys.forEach(key => {
+            redisClient.hgetall(key, (err, obj) => {
+              firstmessageArr.push(obj);
+              console.log("pushing");
+            });
+          });
+        });
+      }
+    })
+    .then(data => {
+      console.log("firstmessageArr:" + firstmessageArr);
+    })
+    .then(data => {
       chatroomMessagesArr = firstmessageArr.filter(obj => {
         if (obj.chatroom === req.params.chatroomName) {
+          console.log("firstmessageArr filtering");
           return true;
         }
       });
-      return chatroomMessagesArr;
+      console.log("chatroomMessagesArr" + chatroomMessagesArr);
     })
-    .then(chatroomMessagesArr => {
-      var allMessageChatrooms = firstmessageArr.map(obj => {
+    .then(data => {
+      allMessageChatrooms = firstmessageArr.map(obj => {
         return obj.chatroom;
       });
-      return allMessageChatrooms;
     })
-    .then(allMessageChatrooms => {
-      var uniqueChatroomsArr = [];
+    .then(data => {
+      uniqueChatroomsArr = [];
       allMessageChatrooms.forEach(item => {
         if (!uniqueChatroomsArr.includes(item)) {
           uniqueChatroomsArr.push(item);
         }
       });
-      return uniqueChatroomsArr;
     })
-    .then(uniqueChatroomsArr => {
+    .then(() => {
       res.render("index", {
         username: req.cookies.username,
         messages: chatroomMessagesArr,
@@ -181,6 +195,68 @@ app.get("/chatroom/:chatroomName", (req, res) => {
       });
     })
     .catch(err => console.log(err));
+  /*
+    //chris
+    app.get("/chatroom/:chatroomName", (req, res) => {
+      let firstmessageArrPromise = () => {
+        return new Promise((resolve, reject) => {
+          redisClient.KEYS("*", (err, keys) => err ? reject(err) : resolve(keys));
+        });
+      };
+
+      let chatroomMessagesArr;
+      let allMessageChatrooms;
+      let uniqueChatroomsArr;
+
+      firstmessageArrPromise()
+        .then(keys => {
+          const promises = [];
+          keys.forEach(key => {
+            const promise = new Promise((resolve, reject) => {
+              redisClient.hgetall(
+                key, (err, obj) => err ? reject(err) : resolve(obj)
+              );
+            });
+            promises.push(promise);
+          });
+          return Promise.all(promises);
+        })
+        .then(values => {
+          // ????
+        })
+        .then(data => {
+          chatroomMessagesArr = firstmessageArr.filter(obj => {
+            if (obj.chatroom === req.params.chatroomName) {
+              console.log("firstmessageArr filtering");
+              return true;
+            }
+          });
+          console.log("chatroomMessagesArr" + chatroomMessagesArr);
+        })
+        .then(data => {
+          allMessageChatrooms = firstmessageArr.map(obj => {
+            return obj.chatroom;
+          });
+        })
+        .then(data => {
+          uniqueChatroomsArr = [];
+          allMessageChatrooms.forEach(item => {
+            if (!uniqueChatroomsArr.includes(item)) {
+              uniqueChatroomsArr.push(item);
+            }
+          });
+        })
+        .then(() => {
+          res.render("index", {
+            username: req.cookies.username,
+            messages: chatroomMessagesArr,
+            chatroomName: req.params.chatroomName,
+            chatroomList: uniqueChatroomsArr
+          });
+        })
+        .catch(err => console.log(err));
+    });
+    */
 });
 
 server.listen(3000);
